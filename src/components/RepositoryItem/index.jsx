01f868ebc50a6152a19a3a5c578d7040b9186afe
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2022-04-10 19:12:40 -0300

    link with api

---
+++ src/components/RepositoryItem/index.jsx
@@ -0,0 +1,14 @@
+
+export function RepositoryItem (props) {
+  return (
+    <>
+      <li>
+        <strong>{props.repository?.name ?? 'Default'}</strong>
+        <p>{props.repository?.description ?? 'Default'}</p>
+        <a href={props.repository?.html_url ?? 'Default'} target='_blank'>
+          {props.repository?.html_url ?? 'Default'}
+        </a>
+      </li>
+    </>
+  )
+}
---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { RepositoryItem } from '../RepositoryItem'
+
+import '../../styles/repositories.scss'
+
+export function RepositoryList () {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    const url = 'https://api.github.com/users/guilhermecardoso93/repos'
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  return (
+    <section className='repository-list'>
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+      </ul>
+    </section>
+  )
+}
